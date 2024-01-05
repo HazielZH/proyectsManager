d950922f1f41d43f220eaaa2d23f00aefb85c9e4
Haziel <holasoyhazi5@gmail.com>
2024-01-05 19:45:37 -0300

    JSON READER, PROFILES DROPDOWN, FLOATING BUTTON

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import flet as ft
 import os
 import subprocess
-
+import json
 
 
 def main(page: ft.Page):
@@ -9,6 +9,9 @@ def main(page: ft.Page):
     page.title = "Proyect Manager"
     codePathChecked = False
     storagePathChecked = False
+    proyectsPath = False
+    profilesVS = []
+    profile = "Default"
     def customDir(e):
         if os.path.exists(e.control.value):
             if "vscode" in e.control.value:
@@ -17,6 +20,9 @@ def main(page: ft.Page):
             if "storage" in e.control.value:
                 page.client_storage.set("StoragePath", e.control.value)
                 storagePathChecked = True
+            else:
+                page.client_storage.set("ProyectsPath", e.control.value)
+                proyectsPath = True
             mainColumn.controls.remove(e.control)
             mainColumn.update()
         else: 
@@ -31,9 +37,7 @@ def main(page: ft.Page):
             codePath = f"C:\\Users\\{osUser}\\AppData\\Local\\Programs\\Microsoft VS Code"
             if os.path.exists(codePath):
                 page.add(ft.Text(value="Existe code"))
-                os.chdir(codePath)
                 page.client_storage.set("CodePath", codePath)
-                subprocess.run('code')
                 codePathChecked = True
             else:
                 textDir = ft.TextField(helper_text="Ingresa el dir de vscode", on_submit=customDir)
@@ -45,7 +49,7 @@ def main(page: ft.Page):
         else:
             osUser = os.getlogin()
             storagePath = f"C:\\Users\\{osUser}\\AppData\\Roaming\\Code\\User\\globalStorage\\storage.json"
-            if os.path.exists(storagePath) and True == False:
+            if os.path.exists(storagePath):
                 page.client_storage.set("StoragePath", storagePath)
                 storagePathChecked = True
             else: 
@@ -53,17 +57,47 @@ def main(page: ft.Page):
                 mainColumn.controls.append(textDir)
                 mainColumn.update()
 
+    def perfJson():
+        Json = open(page.client_storage.get("StoragePath"), 'r')
+        perfData = json.load(Json)
+        for profiles in perfData['userDataProfiles']:
+            dropMenu.options.append(ft.dropdown.Option(profiles['name']))
+        Json.close()
+        print(dropMenu.options)
+        return True
+
+    def checkProyectsPath():
+        if page.client_storage.contains_key("ProyectsPath"):
+            proyectsPath = True
+        else:
+            textDir = ft.TextField(helper_text="Ingresa el dir de tus proyectos", on_submit=customDir)
+            mainColumn.controls.append(textDir)
+            mainColumn.update()
+
+    def changeProfile(e):
+        profile = dropMenu.value
+
+    def ExecuteVSCODE(e):
+        os.chdir(page.client_storage.get('CodePath'))
+        subprocess.run(f'code --profile {dropMenu.value} {page.client_storage.get('ProyectsPath')}')
+
+    dropMenu = ft.Dropdown(
+        on_change=changeProfile,
+        options=[]
+    )
     page.add(
-        mainColumn
+        mainColumn,
+        dropMenu
     )
     if not page.client_storage.contains_key("CodePath"):
         checkCodePath()
     if not page.client_storage.contains_key("StoragePath"):
         checkStoragePath()
-    while True:
-        if checkCodePath:
-            a = 1
-
+    if not page.client_storage.contains_key("ProyectsStorage"):
+        checkProyectsPath()
+    perfJson()
+    dropMenu.update()
+    page.add(ft.FloatingActionButton(ft.icons.PLAY_ARROW_OUTLINED, on_click=ExecuteVSCODE))
 
 
 
